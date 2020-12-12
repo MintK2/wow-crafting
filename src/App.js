21a7123cba994b97b19292011d8c592a3bc36f22
Andrew Westgerdes <ajwestgerdes@gmail.com>
2020-12-12 00:26:44 -0600

    made api request for recipes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import axios from 'axios'
 import {Header} from './components/Header'
 import {Recipe} from './components/Recipe'
@@ -9,7 +9,11 @@ import './App.css';
 
 function App() {
 
+  let itemInput = React.createRef();
+  
   const [token, setToken] = useState("");
+  const [itemID, setItemID] = useState(0);
+  
 
   const postAuth = () => {
     axios.post('https://us.battle.net/oauth/token', null, { params: {
@@ -21,10 +25,30 @@ function App() {
     })
   }
 
+  const getItem = (itemID, token) => {
+    console.log(`https://us.api.blizzard.com/data/wow/recipe/${itemID}?namespace=static-us&locale=en_US&access_token=${token}`)
+    axios.get(`https://us.api.blizzard.com/data/wow/recipe/${itemID}?namespace=static-us&locale=en_US&access_token=${token}`).then((res) => {
+      console.log(res.data)
+    })
+  }
+
+  function handleClick() { 
+    setItemID(itemInput.current.value);
+    console.log(itemID)
+    getItem(itemID, token)
+  }
+
+  postAuth()
+
   return (
     <div>
       < Header/>
-      <button onClick={postAuth}>PostAuth</button>
+      <div>
+      <input ref={itemInput} placeholder="Type a message..." />
+      <button onClick={handleClick} className="icon">
+        <i className="fa fa-play" />
+      </button>
+    </div>
       < Recipe/>
     </div>
   );
